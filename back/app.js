6c8755412ca6950fb9ebc8048f9dfd1e38d9a7af
Gwendal Petitjean <kishador@yahoo.fr>
2022-01-22 16:16:41 +0100

    construction du parcours user

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const userRoutes = require('./routes/user');
 app.use(express.json());
 
 mongoose.connect('mongodb+srv://kishador:pastore27@cluster0.wdbvh.mongodb.net/test?retryWrites=true&w=majority',
@@ -9,6 +10,13 @@ mongoose.connect('mongodb+srv://kishador:pastore27@cluster0.wdbvh.mongodb.net/te
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
+  app.use('/api/auth', userRoutes);
 
 module.exports = app;
