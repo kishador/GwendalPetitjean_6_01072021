eaa8f3a0b630f1bee88ac0e8bb08a9639be99e9f
Gwendal Petitjean <kishador@yahoo.fr>
2022-01-22 16:58:23 +0100

    ajout du parcours sauces

---
+++ back/controllers/controllers/sauces.js
@@ -0,0 +1,50 @@
+const Sauces = require('../models/sauces')
+const fs = require('fs');
+
+exports.createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  const sauce = new Sauces({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}` 
+  });
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Sauce crée!' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.modifySauce = (req, res, next) => {
+  const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Sauces.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+exports.getAllSauce = (req, res, next) => {
+  Sauces.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }))
+}
+
+exports.getOneSauce = (req, res, next) => {
+  Sauces.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json(sauce))
+    .catch(error => res.status(400).json({ error }))
+};
+
+exports.deleteSauce = (req, res, next) => {
+  Sauces.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauces.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Sauce supprimée!' }))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
