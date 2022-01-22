07050e40db5f1d212c1f6663a74e7d9a5fe4aa43
Gwendal Petitjean <kishador@yahoo.fr>
2022-01-22 17:01:00 +0100

    ajout de la fonction like/dislike

--- back/controllers/controllers/sauces.js
+++ back/controllers/controllers/sauces.js
@@ -48,3 +48,53 @@ exports.deleteSauce = (req, res, next) => {
     .catch(error => res.status(500).json({ error }));
 };
 
+
+exports.likeSauce = (req, res, next) => {
+
+  switch (req.body.like) {
+    case 0:
+      Sauces.findOne({ _id: req.params.id })
+        .then((sauce) => {
+          if (sauce.usersLiked.find(user => user === req.body.userId)) {
+            Sauces.updateOne({ _id: req.params.id }, {
+              $inc: { likes: -1 },
+              $pull: { usersLiked: req.body.userId },
+              _id: req.params.id
+            })
+              .then(() => { res.status(201).json({ message: 'Ton avis a été pris en compte!' }); })
+              .catch((error) => { res.status(400).json({ error: error }); });
+
+          } if (sauce.usersDisliked.find(user => user === req.body.userId)) {
+            Sauces.updateOne({ _id: req.params.id }, {
+              $inc: { dislikes: -1 },
+              $pull: { usersDisliked: req.body.userId },
+              _id: req.params.id
+            })
+              .then(() => { res.status(201).json({ message: 'ok...' }); })
+              .catch((error) => { res.status(400).json({ error: error }); });
+          }
+        })
+        .catch((error) => { res.status(404).json({ error: error }); });
+      break;
+    case 1:
+      Sauces.updateOne({ _id: req.params.id }, {
+        $inc: { likes: 1 },
+        $push: { usersLiked: req.body.userId },
+        _id: req.params.id
+      })
+        .then(() => { res.status(201).json({ message: 'Like added!' }); })
+        .catch((error) => { res.status(400).json({ error: error }); });
+      break;
+    case -1:
+      Sauces.updateOne({ _id: req.params.id }, {
+        $inc: { dislikes: 1 },
+        $push: { usersDisliked: req.body.userId },
+        _id: req.params.id
+      })
+        .then(() => { res.status(201).json({ message: 'Ok... it\'\s your right...' }) })
+        .catch((error) => { res.status(400).json({ error: error }) });
+      break;
+    default:
+      console.error('Bad request')
+  }
+};
